cafbf77a29a377264716b41139562ebce24c2f5d
Завьялов Дмитрий Викторович <zdv@mst-company.ru>
2022-12-01 09:10:46 +0300

    new: docker multi lang

--- PythonContestClient/client.py
+++ PythonContestClient/client.py
@@ -12,6 +12,8 @@ class ClientApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
         super().__init__()
         self.setupUi(self)
         self.push_code.clicked.connect(self.select_file)
+        #self.addr = "http://cluster.vstu.ru:57888"
+        self.addr = "http://localhost:57888"
     
     
     async def check_problem(self):
@@ -19,13 +21,13 @@ class ClientApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
         mqtt_key = '123'
         user = self.edit_name.text()
         language = self.edit_language.text()
-        course = 'programming'
+        course = 'test'
         problem = str(self.spin_problem.value())
         variant = str(self.spin_variant.value())
         code = self.text_code.toPlainText()
         message = {'id': id, 'mqtt_key': mqtt_key, 'user': user, 'language': language, 'course': course, 
             'problem': problem, 'variant': variant, 'code': code}
-        resp = requests.post('http://localhost:8000/api/add_message', json=message)
+        resp = requests.post(f'{self.addr}/api/add_message', json=message)
         if not resp.ok:
             self.text_code.setPlainText("Не удалось отправить задачу")
             return
@@ -37,7 +39,7 @@ class ClientApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
             count += 1
             #self.statusbar.showMessage(f"Выполняется попытка №{count}.")
             async with aiohttp.ClientSession() as session:
-                async with session.post(f'http://localhost:8000/api/get_message_result', json=message) as resp:
+                async with session.post(f'{self.addr}/api/get_message_result', json=message) as resp:
                     result = await resp.json()
                 if 'error' not in result:
                     self.text_code.setPlainText(f"Задача проверена.\nРезультат:\n{result}")
@@ -47,7 +49,7 @@ class ClientApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
 
     
     def select_file(self):
-        file_name = QtWidgets.QFileDialog.getOpenFileName(self, "Выбор файла с программой", None, "Python code (*.py)")[0]
+        file_name = QtWidgets.QFileDialog.getOpenFileName(self, "Выбор файла с программой", None, "Python code (*.py)|C# code (*.cs)")[0]
         if not file_name:
             return
         with open(file_name, 'r') as f:
